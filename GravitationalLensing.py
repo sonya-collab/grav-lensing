8d16728db2adff30e508a5485931c499813855ef
Francisco Barreiros Leal de Amorim Resende <ge63fox@mytum.de>
2023-01-11 14:36:07 +0100

    Update GravitationalLensing.py

--- GravitationalLensing.py
+++ GravitationalLensing.py
@@ -1,1 +1,20 @@
 #Hello there! This is just a test! HELLO
+import pytorch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
